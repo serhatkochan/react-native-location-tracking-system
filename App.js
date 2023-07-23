ec3c0cebc4b26d8e2d9dd9a131fb8af3bac8bf1c
Serhat <serhatkochan@hotmail.com.tr>
2023-07-23 19:24:28 +0300

    request location permission and get current location

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [locationError, setLocationError] = useState(null);
+    const [location, setLocation] = useState({lat: null, lon: null});
+
+    useEffect(() => {
+        getLocation();
+    }, []);
+
+    const getLocation = async () => {
+        try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== "granted") {
+                setLocationError("Location permission denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation({lat: location.coords.latitude, lon: location.coords.longitude});
+        } catch (error) {
+            console.error("Error requesting location permission:", error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>{location.lat} - {location.lon}</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default App;
