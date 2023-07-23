a88dc18946d67d7109e1930b4fb9aa3e3f76ab79
Serhat <serhatkochan@hotmail.com.tr>
2023-07-23 21:11:45 +0300

    Login Screen

--- App.js
+++ App.js
@@ -1,59 +1,8 @@
-import {useEffect, useState} from "react";
-import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
-import * as Location from "expo-location";
-import MapView, {Marker} from "react-native-maps";
+import LoginPage from "./src/pages/login-page";
 
 const App = () => {
-    const [locationError, setLocationError] = useState(null);
-    const [location, setLocation] = useState(null);
-
-    useEffect(() => {
-        getLocation();
-    }, []);
-
-    const getLocation = async () => {
-        try {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-
-            if (status !== "granted") {
-                setLocationError("Location permission denied");
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation({...location.coords, latitudeDelta: 0, longitudeDelta: 0.001});
-        } catch (error) {
-            console.error("Error requesting location permission:", error);
-        }
-    };
-
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={{flex: 1}}>
-                <MapView
-                    style={styles.map}
-                    region={location}
-                >
-                    {location && <Marker coordinate={location} />}
-                </MapView>
-            </View>
-            <View style={{flex: 1}}>
-                <Text>{JSON.stringify(location, null, 2)}</Text>
-            </View>
-        </SafeAreaView>
+        <LoginPage />
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 40,
-        backgroundColor: '#fff',
-    },
-    map: {
-        width: '100%',
-        height: '100%',
-    },
-});
-
 export default App;
---
+++ src/pages/login-page/index.js
@@ -0,0 +1,107 @@
+import React from "react";
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput,
+    Dimensions,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import LoginSVG from "../../../assets/login.svg";
+
+const screenWidth = Dimensions.get("window").width;
+const bannerHeight = (screenWidth / 75) * 46; // Image ratio is 75x46
+
+const Login = () => {
+    return (
+        <>
+            <StatusBar style="light" />
+            <SafeAreaView>
+                <LoginSVG {...styles.banner}/>
+            </SafeAreaView>
+
+            <SafeAreaView style={styles.container}>
+                <View style={styles.content}>
+                    <TextInput
+                        style={[styles.input, styles.inputUsername]}
+                        placeholder="Username"
+                        placeholderTextColor="#cdcdcf"
+                    />
+
+                    <TextInput
+                        style={[styles.input, styles.inputPassword]}
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        placeholderTextColor="#cdcdcf"
+                    />
+
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>Location Tracking System</Text>
+                </View>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    banner: {
+        resizeMode: "contain",
+        width: "100%",
+        height: bannerHeight,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+    },
+    content: {
+        padding: 22,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#cdcdcf",
+        color: "#333333",
+        fontSize: 16,
+        height: 44,
+        paddingHorizontal: 15,
+    },
+    inputUsername: {
+        borderBottomWidth: 0,
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
+    },
+    inputPassword: {
+        borderBottomLeftRadius: 3,
+        borderBottomRightRadius: 3,
+    },
+    button: {
+        height: 42,
+        borderRadius: 6,
+        backgroundColor: "#4e7c86",
+        justifyContent: "center",
+        marginVertical: 15,
+    },
+    buttonText: {
+        color: "#e0e4e8",
+        textAlign: "center",
+        fontSize: 16,
+    },
+    footer: {
+        alignItems: "center",
+        padding: 22,
+        paddingBottom: 0,
+        marginBottom: 10,
+    },
+    footerText: {
+        textAlign: "center",
+    },
+});
+
+
+export default Login;
